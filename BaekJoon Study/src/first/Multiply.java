79cc135b41d8eab87064c95cb78a883d3fccc923
Seong-Jun1525 <sjsj123455@naver.com>
2022-04-08 20:38:16 +0900

    1-7

---
+++ BaekJoon Study/src/first/Multiply.java
@@ -0,0 +1,18 @@
+package first;
+
+import java.util.Scanner;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		int x;
+        int y;
+        
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        y = sc.nextInt();
+        
+        System.out.println(x*y);
+	}
+
+}
