36dbdaf9841ed5612dc429ba5e647563efc2832b
Seong-Jun1525 <sjsj123455@naver.com>
2022-04-08 20:36:48 +0900

    1-6

---
+++ BaekJoon Study/src/first/Minus.java
@@ -0,0 +1,19 @@
+package first;
+
+import java.util.Scanner;
+
+public class Minus {
+
+	public static void main(String[] args) {
+		int x;
+        int y;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        x = sc.nextInt();
+        y = sc.nextInt();
+        
+        System.out.println(x-y);
+	}
+
+}
