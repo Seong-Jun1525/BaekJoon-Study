b0231b5ce387e225f3d7d74177f20eadedabb188
Seong-Jun1525 <sjsj123455@naver.com>
2022-04-08 20:35:14 +0900

    1-5

---
+++ BaekJoon Study/src/first/Plus_1.java
@@ -0,0 +1,19 @@
+package first;
+
+import java.util.Scanner;
+
+public class Plus_1 {
+
+	public static void main(String[] args) {
+		int x;
+        int y;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        x = sc.nextInt();
+        y = sc.nextInt();
+        
+        System.out.println(x+y);
+	}
+
+}
