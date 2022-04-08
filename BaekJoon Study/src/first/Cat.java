e1d6d5ce441aaffcfb2caf22e3cbc40191b2c67f
Seong-Jun1525 <sjsj123455@naver.com>
2022-04-08 20:33:24 +0900

    1-3

---
+++ BaekJoon Study/src/first/Cat.java
@@ -0,0 +1,12 @@
+package first;
+
+public class Cat {
+
+	public static void main(String[] args) {
+		System.out.println( "\\    /\\\n" + 
+				" )  ( ')\n" + 
+				"(  /  )\n" + 
+				" \\(__)|");
+	}
+
+}
