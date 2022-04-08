c36e20b275ae97c441ab332e7a0a02419512f754
Seong-Jun1525 <sjsj123455@naver.com>
2022-04-08 20:33:49 +0900

    1-4

---
+++ BaekJoon Study/src/first/Dog.java
@@ -0,0 +1,13 @@
+package first;
+
+public class Dog {
+
+	public static void main(String[] args) {
+		System.out.println("|\\_/|\n"
+	            + "|q p|   /}\n"
+	            + "( 0 )\"\"\"\\\n"
+	            + "|\"^\"`    |\n"
+	            + "||_/=\\\\__|");
+	}
+
+}
