7e8d72d7321a3c5a92ff00cda117293014435a65
Seong-Jun1525 <sjsj123455@naver.com>
2022-04-10 14:47:29 +0900

    3-4, 3-5, 3-6

---
+++ BaekJoon Study/src/third/FastPlus.java
@@ -0,0 +1,29 @@
+package third;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class FastPlus {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st;
+		
+		int testCase = Integer.parseInt(br.readLine());
+		
+		
+		for(int i = 0; i < testCase; i++) {
+			st = new StringTokenizer(br.readLine());
+			bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+		}
+		
+		bw.close();
+	}
+
+}
